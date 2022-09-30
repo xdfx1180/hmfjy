e1d5124c9b20c5f76c1bf78f998765bcb1881fc6
xdfx <45113856+xdfx1180@users.noreply.github.com>
2022-10-01 00:26:56 +0800

    Update 18av.js

--- js/18av.js
+++ js/18av.js
@@ -6,8 +6,8 @@ var rule = {
         'User-Agent':'MOBILE_UA'
     },
     timeout:5000,
-    class_name:'中文字幕&无码AV&自拍影片&动画&近亲相奸&通奸&出轨&人妻&寡妇&秘书&义母&按摩&母乳&人妻・主妇',//静态分类名称拼接
-    class_url:'chinese_random&/uncensored_random&dt_random&animation_random&chinese_category/7/近親相姦&chinese_category/21/通姦&chinese_category/34/出軌&chinese_category/72/人妻&chinese_category/75/寡婦&chinese_category/81/秘書&chinese_category/87/義母&chinese_category/162/按摩&chinese_category/164/母乳&chinese_category/299/人妻・主婦',//静态分类标识拼接
+    class_name:'每日更新&中文字幕&无码AV&自拍影片&动画&近亲相奸&通奸&出轨&人妻&寡妇&秘书&义母&按摩&母乳&人妻・主妇',//静态分类名称拼接
+    class_url:'chinese_list&chinese_random&/uncensored_random&dt_random&animation_random&chinese_category/7/近親相姦&chinese_category/21/通姦&chinese_category/34/出軌&chinese_category/72/人妻&chinese_category/75/寡婦&chinese_category/81/秘書&chinese_category/87/義母&chinese_category/162/按摩&chinese_category/164/母乳&chinese_category/299/人妻・主婦',//静态分类标识拼接
    //class_parse:'ul.animenu__nav&&li;a&&Text;a&&href',
     limit:5,
     play_parse:true,
